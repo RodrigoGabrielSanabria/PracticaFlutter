6e7a37c5ee46a457114e69315e39447c9b017b31
Rodrigo Gabriel Sanabria <sanabriarodrigo50@gmail.com>
2024-11-04 14:43:08 -0300

    Update main.dart

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CounterScreen()
+  
+      home:  CounterScreen()
     );
   }
 }
