bb2a82f360c85d96fc2ae2d01bcfbcffaf45abb1
Rodrigo Gabriel Sanabria <sanabriarodrigo50@gmail.com>
2024-11-06 11:45:09 -0300

    Update chat_screen.dart

--- yes_no_app/lib/presentation/chat/chat_screen.dart
+++ yes_no_app/lib/presentation/chat/chat_screen.dart
@@ -14,6 +14,7 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(4.0),
           child: CircleAvatar(
             backgroundImage: NetworkImage('https://media.gq.com.mx/photos/5ef2447d8f739bf973bc8588/16:9/w_2560%2Cc_limit/megan-fox-auto-.jpg') ,
+            
           ),
         ),
         title: const Text('Mi amor ❤️'),
