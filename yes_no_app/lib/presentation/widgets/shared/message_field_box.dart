af13e4f9e260f014c91071142effb1a91150de4a
Rodrigo Gabriel Sanabria <sanabriarodrigo50@gmail.com>
2024-11-06 20:19:42 -0300

    Create message_field_box.dart

---
+++ yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MessageFielBox extends StatelessWidget {
+  const MessageFielBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final textController= TextEditingController();
+    final focusNode = FocusNode();
+
+    final outlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide( color: Colors.transparent),
+      borderRadius: BorderRadius.circular(40)
+    );
+
+    final inputDecoration = InputDecoration(  //bordes para la caja de texto con las dimenciones ya cargadas
+        hintText: 'End your message with a "?"' ,
+        enabledBorder: outlineInputBorder,
+        focusedBorder: outlineInputBorder,
+        filled: true,
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.send_outlined),
+          onPressed: () {
+            final textValue = textController.value.text;
+            print('button: $textValue');
+            textController.clear(); //Limpia la caja de texto al enviar
+          },
+        ),
+      );
+
+    return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print('Sumit value $value');
+        textController.clear(); //Limpia la caja de texto al enviar
+        focusNode.requestFocus(); //Pasa el foco al siguiente widget
+      },
+      
+    );
+  }
+}
