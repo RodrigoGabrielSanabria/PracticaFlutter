ee4fb031f1427ee65763efab90facdbe6fd816bd
Rodrigo Gabriel Sanabria <sanabriarodrigo50@gmail.com>
2024-11-11 19:07:39 -0300

    Update main.dart

--- yes_no_app/lib/main.dart
+++ yes_no_app/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'package:yes_no_app/config/theme/app_theme.dart';
 import 'package:yes_no_app/presentation/Screens/chat/chat_screen.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,11 +12,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Yes No App',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 2).theme(),
-      home: const ChatScreen()
+    return MultiProvider(
+      providers:[
+        ChangeNotifierProvider(create: (_)=>ChatProvider())
+      ],
+      child: MaterialApp(
+        title: 'Yes No App',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme(selectedColor: 2).theme(),
+        home: const ChatScreen()
+      ),
     );
   }
 }
