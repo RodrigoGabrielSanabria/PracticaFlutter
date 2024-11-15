367e4e4d859961785046e43f901561de2f5708b3
Rodrigo Gabriel Sanabria <sanabriarodrigo50@gmail.com>
2024-11-15 19:32:54 -0300

    Update chat_provider.dart

--- yes_no_app/lib/presentation/providers/chat_provider.dart
+++ yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entitie/message.dart';
 
 
 class ChatProvider extends ChangeNotifier{
 
 final ScrollController chatScrollController = ScrollController();
+final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
 
 List<Message>messageList =[
 
@@ -19,10 +21,21 @@ Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me),
    final newMessage = Message(text: text, fromWho: FromWho.me);
    messageList.add(newMessage);
 
+    if (text.endsWith('?')){
+       herReply();  //si el mensaje es una pregunta, se responde automaticamente con una respuesta aleatoria
+    }
    notifyListeners();
    
    moveScrollToBottom();
    
+ }
+
+ Future <void> herReply() async{
+  final herMessage = await getYesNoAnswer.getAnswer();  //El mensaje se lanza cuando detencta que el mensaje recibido es una pregunta
+  messageList.add(herMessage);
+  notifyListeners();
+
+   moveScrollToBottom();
  }
 
  Future <void> moveScrollToBottom() async{
