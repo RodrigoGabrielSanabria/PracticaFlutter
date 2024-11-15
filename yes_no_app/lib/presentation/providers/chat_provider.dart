28c3a6bf293ba3c3007c45e1f229f07110592f95
Rodrigo Gabriel Sanabria <sanabriarodrigo50@gmail.com>
2024-11-15 18:29:22 -0300

    Update chat_provider.dart
    Se agrego la opción de Scroll en la pantalla e intervalos para que el UI se actualice

--- yes_no_app/lib/presentation/providers/chat_provider.dart
+++ yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -11,17 +11,30 @@ List<Message>messageList =[
 Message(text: 'Hola amor!', fromWho: FromWho.me),
 Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me),
 
-] ;
+];
 
-Future<void> sendMessage(String text) async {
+  Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;   //evita que se manden mensajes vacios
 
    final newMessage = Message(text: text, fromWho: FromWho.me);
    messageList.add(newMessage);
 
    notifyListeners();
- 
-}
+   
+   moveScrollToBottom();
+   
+ }
 
+ Future <void> moveScrollToBottom() async{
 
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    chatScrollController.animateTo(
+     chatScrollController.position.maxScrollExtent + chatScrollController.position.maxScrollExtent*0.3,           //posicion
+     duration: const Duration(milliseconds: 300),          //duracion de la animacion
+      curve: Curves.easeOut
+    );       //tipo de animacion
+
+  }
 
 }
